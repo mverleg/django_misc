e7434ed83e780a98075e9f7a74cf2a3c80b357d3
rafiki <mark@rafiki>
2015-10-15 00:39:07 +0200

    now independent of muuser

--- models.py
+++ models.py
@@ -5,17 +5,11 @@
 	no extra queries, but queries do get slower (more joins)
 """
 
-from model_utils.managers import InheritanceManager, InheritanceManagerMixin
-from muuser.models.user import MuUserManager
+from model_utils.managers import InheritanceManager
 
 
 class AutoSubclassManager(InheritanceManager):
 	def get_queryset(self):
 		return super(AutoSubclassManager, self).get_queryset().select_subclasses()
-
-
-class MuUserAutoSubclassManager(InheritanceManagerMixin, MuUserManager):
-	def get_queryset(self):
-		return super(MuUserAutoSubclassManager, self).get_queryset().select_subclasses()
 
 
