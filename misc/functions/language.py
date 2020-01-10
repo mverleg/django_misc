a39b2d1b797131e8d961ee16faa5b6f5ea2c9527
Mark <mverleg.noreply@gmail.com>
2020-01-11 00:08:29 +0100

    Forgot to commit one change

--- misc/functions/language.py
+++ misc/functions/language.py
@@ -6,19 +6,19 @@ from xpinyin import Pinyin
 xpinyin = Pinyin()
 
 
-def to_pinyin(cny, delim=None, splitter=' ', show_tone_marks=True):
+def to_pinyin(cny, delim=None, splitter=' ', tone_marks=True):
 	if delim is not None:
 		splitter = delim
 		warning('"delim" argument is now called "splitter"')
-	return xpinyin.get_pinyin(cny, splitter=splitter, show_tone_marks=show_tone_marks)
+	return xpinyin.get_pinyin(cny, splitter=splitter, tone_marks=tone_marks)
 
 
-def to_pinyin_ascii(cny, splitter=' ', show_tone_marks=False):
-	return to_pinyin(cny, splitter=splitter, show_tone_marks=show_tone_marks)
+def to_pinyin_ascii(cny, splitter=' ', tone_marks=False):
+	return to_pinyin(cny, splitter=splitter, tone_marks=tone_marks)
 
 
-def to_pinyin_full(cny, splitter='', show_tone_marks=True):
-	return to_pinyin(cny, splitter=splitter, show_tone_marks=show_tone_marks)
+def to_pinyin_full(cny, splitter='', tone_marks=True):
+	return to_pinyin(cny, splitter=splitter, tone_marks=tone_marks)
 
 
 if __name__ == '__main__':
